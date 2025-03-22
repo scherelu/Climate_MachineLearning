9ec0b990187d2d1fb280f313f4eb7769307f0298
Ludwig Scherer <schererludwig@gmail.com>
2025-03-21 22:55:46 -0600

    added links to original datasets

--- DataSet_Manipulation.py
+++ DataSet_Manipulation.py
@@ -4,6 +4,19 @@ The annual data set is already merged, the monthly dataset is still work in prog
 that we need to hand this in aswell. While I do have a record of how I manipulated the datafiles, I did not have
 time to reconstruct all of the python code that went into it. The methods how we attained our final datasets
 will of course be included in our final report for transparency.
+
+Original Datasets:
+
+    sea_ice: https://www.kaggle.com/datasets/nsidcorg/daily-sea-ice-extent-data
+    ave_temp_change: https://www.kaggle.com/datasets/sevgisarac/temperature-change
+    co2 monthly mean: https://gml.noaa.gov/webdata/ccgg/trends/co2/co2_mm_gl.csv
+    co2 annual mean: https://gml.noaa.gov/webdata/ccgg/trends/co2/co2_annmean_gl.csv
+    co2 annual growth: https://gml.noaa.gov/webdata/ccgg/trends/co2/co2_gr_gl.csv
+    ch4 monthly mean: https://gml.noaa.gov/webdata/ccgg/trends/ch4/ch4_mm_gl.csv
+    ch4 annual mean: https://gml.noaa.gov/webdata/ccgg/trends/ch4/ch4_annmean_gl.csv
+    ch4 annual growth: https://gml.noaa.gov/webdata/ccgg/trends/ch4/ch4_gr_gl.csv
+    climate insight data: https://www.kaggle.com/datasets/goyaladi/climate-insights-dataset
+
 """
 
 import pandas as pd
--- README.md
+++ README.md
@@ -6,7 +6,12 @@
 
     sea_ice: https://www.kaggle.com/datasets/nsidcorg/daily-sea-ice-extent-data
     ave_temp_change: https://www.kaggle.com/datasets/sevgisarac/temperature-change
-    co2 and ch4 datasets: https://gml.noaa.gov/ccgg/trends/  
+    co2 monthly mean: https://gml.noaa.gov/webdata/ccgg/trends/co2/co2_mm_gl.csv
+    co2 annual mean:https://gml.noaa.gov/webdata/ccgg/trends/co2/co2_annmean_gl.csv
+    co2 annual growth:https://gml.noaa.gov/webdata/ccgg/trends/co2/co2_gr_gl.csv
+    ch4 monthly mean:https://gml.noaa.gov/webdata/ccgg/trends/ch4/ch4_mm_gl.csv
+    ch4 annual mean:https://gml.noaa.gov/webdata/ccgg/trends/ch4/ch4_annmean_gl.csv
+    ch4 annual growth:https://gml.noaa.gov/webdata/ccgg/trends/ch4/ch4_gr_gl.csv 
 
 ### Summary:
     The idea is to essentially gather two types of data sets: growthrate, and absolute value. For each of the factors 
